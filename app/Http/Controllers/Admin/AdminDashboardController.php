84a98ef380e2548cace9ed5bfe04769e4674ebbd
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2024-09-09 12:49:57 +0600

    Admin Dashboard

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -3,11 +3,39 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use App\Models\Destination;
+use App\Models\Package;
+use App\Models\Slider;
+use App\Models\Subscriber;
+use App\Models\TeamMember;
+use App\Models\Testimonial;
+use App\Models\Tour;
+use App\Models\User;
 
 class AdminDashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $total_slider = Slider::count();
+        $total_testimonial = Testimonial::count();
+        $total_team_member = TeamMember::count();
+        $total_blog_post = Blog::count();
+        $total_destination = Destination::count();
+        $total_package = Package::count();
+        $total_user = User::where('status', 1)->count();
+        $total_subscriber = Subscriber::where('status', 'Active')->count();
+        $total_tour = Tour::count();
+        return view('admin.dashboard', compact([
+            'total_slider',
+            'total_testimonial',
+            'total_team_member',
+            'total_blog_post',
+            'total_destination',
+            'total_package',
+            'total_user',
+            'total_subscriber',
+            'total_tour',
+        ]));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,7 +5,123 @@
 @push('admin_style')
 @endpush
 @section('content')
-    <h1>Hi This is Admin Dashboard</h1>
+    <div class="col-lg-3 col-md-3 col-sm-6 mb-4">
+        <div class="card ">
+            <div class="card-body">
+                <div class="card-title d-flex align-items-start justify-content-between">
+                    <div class="avatar flex-shrink-0">
+                        <i class="menu-icon tf-icons bx bx-slider"></i>
+                    </div>
+                </div>
+                <span class="fw-semibold d-block mb-1">Total Sliders</span>
+                <h3 class="card-title mb-2">{{ $total_slider }}</h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 mb-4">
+        <div class="card ">
+            <div class="card-body">
+                <div class="card-title d-flex align-items-start justify-content-between">
+                    <div class="avatar flex-shrink-0">
+                        <i class="menu-icon tf-icons bx bx-comment"></i>
+                    </div>
+                </div>
+                <span class="fw-semibold d-block mb-1">Total Testimonials</span>
+                <h3 class="card-title mb-2">{{ $total_testimonial }}</h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 mb-4">
+        <div class="card ">
+            <div class="card-body">
+                <div class="card-title d-flex align-items-start justify-content-between">
+                    <div class="avatar flex-shrink-0">
+                        <i class="menu-icon tf-icons bx bx-user-circle"></i>
+                    </div>
+                </div>
+                <span class="fw-semibold d-block mb-1">Total Team Members</span>
+                <h3 class="card-title mb-2">{{ $total_team_member }}</h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 mb-4">
+        <div class="card ">
+            <div class="card-body">
+                <div class="card-title d-flex align-items-start justify-content-between">
+                    <div class="avatar flex-shrink-0">
+                        <i class="menu-icon tf-icons bx bx-podcast"></i>
+                    </div>
+                </div>
+                <span class="fw-semibold d-block mb-1">Total Posts</span>
+                <h3 class="card-title mb-2">{{ $total_blog_post }}</h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 mb-4">
+        <div class="card ">
+            <div class="card-body">
+                <div class="card-title d-flex align-items-start justify-content-between">
+                    <div class="avatar flex-shrink-0">
+                        <i class="menu-icon tf-icons bx bx-current-location"></i>
+                    </div>
+                </div>
+                <span class="fw-semibold d-block mb-1">Total Destinations</span>
+                <h3 class="card-title mb-2">{{ $total_destination }}</h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 mb-4">
+        <div class="card ">
+            <div class="card-body">
+                <div class="card-title d-flex align-items-start justify-content-between">
+                    <div class="avatar flex-shrink-0">
+                        <i class="menu-icon tf-icons bx bx-package"></i>
+                    </div>
+                </div>
+                <span class="fw-semibold d-block mb-1">Total Packages</span>
+                <h3 class="card-title mb-2">{{ $total_package }}</h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 mb-4">
+        <div class="card ">
+            <div class="card-body">
+                <div class="card-title d-flex align-items-start justify-content-between">
+                    <div class="avatar flex-shrink-0">
+                        <i class="menu-icon tf-icons bx bx-user"></i>
+                    </div>
+                </div>
+                <span class="fw-semibold d-block mb-1">Total Users</span>
+                <h3 class="card-title mb-2">{{ $total_user }}</h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 mb-4">
+        <div class="card ">
+            <div class="card-body">
+                <div class="card-title d-flex align-items-start justify-content-between">
+                    <div class="avatar flex-shrink-0">
+                        <i class="menu-icon tf-icons bx bxs-user-badge"></i>
+                    </div>
+                </div>
+                <span class="fw-semibold d-block mb-1">Total Subscribers</span>
+                <h3 class="card-title mb-2">{{ $total_subscriber }}</h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 mb-4">
+        <div class="card ">
+            <div class="card-body">
+                <div class="card-title d-flex align-items-start justify-content-between">
+                    <div class="avatar flex-shrink-0">
+                        <i class="menu-icon tf-icons bx bxs-plane"></i>
+                    </div>
+                </div>
+                <span class="fw-semibold d-block mb-1">Total Tours</span>
+                <h3 class="card-title mb-2">{{ $total_tour }}</h3>
+            </div>
+        </div>
+    </div>
 @endsection
 @push('admin_script')
 @endpush
